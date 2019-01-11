c0794eaa233811f24016adced9c59558c0b920f0
sohag2018 <nafasatzayan@gmail.com>
2019-01-11 10:17:37 -0500

    newGroup13

--- Costco/src/main/java/homePage/MainPage.java
+++ Costco/src/main/java/homePage/MainPage.java
@@ -7,71 +7,186 @@ import org.openqa.selenium.support.FindBy;
 import reporting.TestLogger;
 
 public class MainPage extends CommonAPI {
-    @FindBy(xpath = "//img[@alt='Mount Sinai']")
+    @FindBy(id = "warehouse-savings")
+    public static WebElement viewWareHouseSavings;
+    @FindBy(id="warehouse-locations-dropdown")
+    public static WebElement FindaWarehouse;
+    @FindBy(id="email-signup-dropdown")
+    public static WebElement GetEmailOffers;
+    @FindBy(id="customer-service-link")
+    public static WebElement CustomerService;
+    @FindBy(xpath ="//a[@id='country-select']" )
+    public static WebElement us;
+    @FindBy(xpath ="//div[@class='table-cell logo hidden-xs hidden-sm hidden-md hidden-lg']//img[@class='bc-logo logo-us']" )
     public static WebElement logo;
-    @FindBy(linkText = "Find a Doctor")
-    public static WebElement findADoctor;
-    @FindBy(id = "hploc__sel-text-hosp")
-    public static WebElement selectHospitals1;
-    @FindBy(xpath = "//h3[contains(text(),'300+ Community Locations')]")
-    public static WebElement communityLocations300Plus;
-    @FindBy(xpath = "//span[contains(text(),'You can access Mount Sinai health care across New York City and beyond')]")
-    public static WebElement captionOfSymbolOfMap;
-    @FindBy(xpath = "//div[@class='nav navbar col-xs-12']//a[text()='Inside Mount Sinai Blog']")
-    public static WebElement insideMountSinaiBlog;
-    @FindBy(xpath = "//i[@class='fa fa-linkedin']")
-    public static WebElement blogInLinkedIn;
-    @FindBy(xpath = "//div[@class='nav col-xs-12 col-sm-4']//a[contains(text(),'International Services')]")
-    public static WebElement internationalServices;
-    @FindBy(xpath = "//div[@class='nav col-xs-12 col-sm-4']//a[text()='Find a Doctor']")
-    public static WebElement findADoctor3;
+    @FindBy(id="search-dropdown-select")
+    public static WebElement All;
+    @FindBy(xpath ="//input[@id='search-field']" )
+    public static WebElement search;
+    @FindBy(id="header_sign_in")
+    public static WebElement SignInRegister;
+    @FindBy(id="cart-d")
+    public static WebElement Cart;
+    @FindBy(id="navigation-dropdown")
+    public static WebElement ShopAllDepartment;
+    @FindBy(xpath="//a[@id='Home_Ancillary_0']")
+    public static WebElement Grocery;
+    @FindBy(xpath="//a[@id='Home_Ancillary_1']")
+    public static WebElement BusinessDelivery;
+    @FindBy(xpath="//a[@id='Home_Ancillary_2']")
+    public static WebElement Optical;
+    @FindBy(xpath="//a[@id='Home_Ancillary_3']")
+    public static WebElement Pharmacy;
+    @FindBy(xpath="//a[@id='Home_Ancillary_4']")
+    public static WebElement Services;
+    @FindBy(xpath="//a[@id='Home_Ancillary_5']")
+    public static WebElement Photo;
+    @FindBy(xpath="//a[@id='Home_Ancillary_6']")
+    public static WebElement Travel;
+    @FindBy(xpath="//a[@id='Home_Ancillary_7']")
+    public static WebElement Membership;
+    @FindBy(xpath="//a[@id='Home_Ancillary_0']")
+    public static WebElement Location; // Have to fix
+    @FindBy(xpath="//div[text()='Customer Service']")
+    public static WebElement customerService;
+    @FindBy(className="get-help")
+    public static WebElement getHelp;
+    @FindBy(id="footer-find-warehouse-block")
+    public static WebElement findaWarehouse;
+    @FindBy(id="footer-email-offers-block")
+    public static WebElement getGetEmailOffers;
+    @FindBy(xpath="//input[@id='footer-search-field']")
+    public static WebElement cityStateZip;
+    @FindBy(xpath="//input[@id='footer-email-offers']")
+    public static WebElement enterYourEmail;
+    @FindBy(xpath="//div[text()='Follow Us']")
+    public static WebElement followUs;
+    @FindBy(xpath="//ul[@class='social']//i[@class='fi-social-facebook']")
+    public static WebElement facebookLogo;
+    @FindBy(xpath="//ul[@class='social']//i[@class='fi-social-pinterest']")
+    public static WebElement pLogo;
 
 
-
-    public void checkLogo() {
-        TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object() {
-        }.getClass().getEnclosingMethod().getName()));
+    public void checkviewWareHouseSavings(){
+        TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object(){}.getClass().getEnclosingMethod().getName()));
+        viewWareHouseSavings.click();
+    }
+    public void checkFindaWarehouse(){
+        TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object(){}.getClass().getEnclosingMethod().getName()));
+        FindaWarehouse.click();
+    }
+    public void checkGetEmailOffers(){
+        TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object(){}.getClass().getEnclosingMethod().getName()));
+        GetEmailOffers.click();
+    }
+    public void checkCustomerService(){
+        TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object(){}.getClass().getEnclosingMethod().getName()));
+        CustomerService.click();
+    }
+    public void checkus(){
+        TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object(){}.getClass().getEnclosingMethod().getName()));
+        us.click();
+    }
+    public void checkLogo(){
+        TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object(){}.getClass().getEnclosingMethod().getName()));
         logo.click();
     }
-    public void checkFindADoctor() {
-        TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object() {
-        }.getClass().getEnclosingMethod().getName()));
-        findADoctor.click();
+    public void checkAll(){
+        TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object(){}.getClass().getEnclosingMethod().getName()));
+        All.click();
     }
-    public void checkSelectHospitals1() {
-        TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object() {
-        }.getClass().getEnclosingMethod().getName()));
-        selectHospitals1.click();
+    public void checkSearch(){
+        TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object(){}.getClass().getEnclosingMethod().getName()));
+        search.click();
     }
-    public void checkCommunityLocations300Plus() {
-        TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object() {
-        }.getClass().getEnclosingMethod().getName()));
-        communityLocations300Plus.click();
+    public void checkSignInRegister(){
+        TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object(){}.getClass().getEnclosingMethod().getName()));
+        SignInRegister.click();
     }
-    public void checkCaptionOfSymbolOfMap() {
-        TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object() {
-        }.getClass().getEnclosingMethod().getName()));
-        captionOfSymbolOfMap.click();
+    public void checkCart(){
+        TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object(){}.getClass().getEnclosingMethod().getName()));
+        Cart.click();
     }
-    public void checkInsideMountSinaiBlog() {
-        TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object() {
-        }.getClass().getEnclosingMethod().getName()));
-        insideMountSinaiBlog.click();
+    public void checkGrocery(){
+        TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object(){}.getClass().getEnclosingMethod().getName()));
+        Grocery.click();
     }
-    public void checkBlogInLinkedIn() {
-        TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object() {
-        }.getClass().getEnclosingMethod().getName()));
-        blogInLinkedIn.click();
+    public void checkShopAllDepartment(){
+        TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object(){}.getClass().getEnclosingMethod().getName()));
+        Grocery.click();
     }
-    public void checkInternationalServices() {
-        TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object() {
-        }.getClass().getEnclosingMethod().getName()));
-        internationalServices.click();
+    public void checkBusinessDelivery(){
+        TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object(){}.getClass().getEnclosingMethod().getName()));
+        BusinessDelivery.click();
     }
-    public void checkFindADoctor3() {
-        TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object() {
-        }.getClass().getEnclosingMethod().getName()));
-        findADoctor3.click();
+    public void checkOptical(){
+        TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object(){}.getClass().getEnclosingMethod().getName()));
+        Optical.click();
     }
+    public void checkPharmacy(){
+        TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object(){}.getClass().getEnclosingMethod().getName()));
+        Pharmacy.click();
+    }
+    public void checkServices(){
+        TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object(){}.getClass().getEnclosingMethod().getName()));
+        Services.click();
+    }
+    public void checkPhoto(){
+        TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object(){}.getClass().getEnclosingMethod().getName()));
+        Photo.click();
+    }
+    public void checkTravel(){
+        TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object(){}.getClass().getEnclosingMethod().getName()));
+        Travel.click();
+    }
+    public void checkMembership(){
+        TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object(){}.getClass().getEnclosingMethod().getName()));
+        Membership.click();
+    }
+    public void checkLocation(){
+        TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object(){}.getClass().getEnclosingMethod().getName()));
+        Location.click();
+    }
+    public void customerService(){
+        TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object(){}.getClass().getEnclosingMethod().getName()));
+        customerService.click();
+    }
+    public void getHelp(){
+        TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object(){}.getClass().getEnclosingMethod().getName()));
+        getHelp.click();
+    }
+    public void findaWarehouse(){
+        TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object(){}.getClass().getEnclosingMethod().getName()));
+        findaWarehouse.click();
+    }
+
+    public void getGetEmailOffers(){
+        TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object(){}.getClass().getEnclosingMethod().getName()));
+        getGetEmailOffers.click();
+    }
+    public void cityStateZip(){
+        TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object(){}.getClass().getEnclosingMethod().getName()));
+        cityStateZip.click();
+    }
+    public void enterYourEmail(){
+        TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object(){}.getClass().getEnclosingMethod().getName()));
+        enterYourEmail.click();
+    }
+    public void followUs(){
+        TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object(){}.getClass().getEnclosingMethod().getName()));
+        followUs.click();
+    }
+    public void facebookLogo(){
+        TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object(){}.getClass().getEnclosingMethod().getName()));
+        facebookLogo.click();
+    }
+    public void pLogo(){
+        TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object(){}.getClass().getEnclosingMethod().getName()));
+        pLogo.click();
+    }
+
+
+
+
 
 }
--- Costco/src/test/java/testHomePage/TestMainPage.java
+++ Costco/src/test/java/testHomePage/TestMainPage.java
@@ -10,54 +10,191 @@ public class TestMainPage extends MainPage {
 
     MainPage mainPage;
     @BeforeMethod
-    public void initialize() {
-        TestLogger.log(convertToString(new Object(){}.getClass().getSimpleName()) + " : " + convertToString(new Object() {
-        }.getClass().getEnclosingMethod().getName())); //implemented by Nasir
+    public void initElements() {
+        TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object() {
+        }.getClass().getEnclosingMethod().getName()));
         mainPage = PageFactory.initElements(driver, MainPage.class);
     }
     @Test
-    public void testLogo(){
-        TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object(){}.getClass().getEnclosingMethod().getName()));
+    public void testviewWareHouseSavings() {
+        TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object() {
+        }.getClass().getEnclosingMethod().getName()));
+        mainPage.checkviewWareHouseSavings();
+    }
+    @Test
+    public void testFindaWarehouse() {
+        TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object() {
+        }.getClass().getEnclosingMethod().getName()));
+        mainPage.checkFindaWarehouse();
+    }
+    @Test
+    public void testGetEmailOffers() {
+        TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object() {
+        }.getClass().getEnclosingMethod().getName()));
+        mainPage.checkGetEmailOffers();
+    }
+    @Test
+    public void testCustomerService() {
+        TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object() {
+        }.getClass().getEnclosingMethod().getName()));
+        mainPage.checkCustomerService();
+    }
+    @Test
+    public void testus() {
+        TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object() {
+        }.getClass().getEnclosingMethod().getName()));
+        mainPage.checkus();
+    }
+    @Test
+    public void testLogo() {
+        TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object() {
+        }.getClass().getEnclosingMethod().getName()));
         mainPage.checkLogo();
     }
     @Test
-    public void testFindADoctor(){
-        TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object(){}.getClass().getEnclosingMethod().getName()));
-        mainPage.checkFindADoctor();
-    }
-    @Test
-    public void testCommunityLocations300Plus(){
-        TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object(){}.getClass().getEnclosingMethod().getName()));
-        mainPage.checkCommunityLocations300Plus();
-    }
-    @Test
-    public void testCaptionOfSymbolOfMap() {
+    public void testAll() {
         TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object() {
         }.getClass().getEnclosingMethod().getName()));
-        mainPage.checkCaptionOfSymbolOfMap();
+        mainPage.checkAll();
     }
     @Test
-    public void testInsideMountSinaiBlog() {
+    public void testSearch() {
         TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object() {
         }.getClass().getEnclosingMethod().getName()));
-        mainPage.checkInsideMountSinaiBlog();
+        mainPage.checkSearch();
     }
     @Test
-    public void testBlogInLinkedIn() {
+    public void testSignInRegister() {
         TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object() {
         }.getClass().getEnclosingMethod().getName()));
-        mainPage.checkBlogInLinkedIn();
+        mainPage.checkSignInRegister();
     }
     @Test
-    public void testInternationalServices() {
+    public void testCart() {
         TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object() {
         }.getClass().getEnclosingMethod().getName()));
-        mainPage.checkInternationalServices();
+        mainPage.checkCart();
     }
     @Test
-    public void testFindADoctor3() {
+    public void testShopAllDepartment() {
         TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object() {
         }.getClass().getEnclosingMethod().getName()));
-        mainPage.checkFindADoctor3();
+        mainPage.checkShopAllDepartment();
     }
+    @Test
+    public void testGrocery() {
+        TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object() {
+        }.getClass().getEnclosingMethod().getName()));
+        mainPage.checkGrocery();
+    }
+    @Test
+    public void testBusinessDelivery() {
+        TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object() {
+        }.getClass().getEnclosingMethod().getName()));
+        mainPage.checkBusinessDelivery();   //takes longer time to check. Need to fix it
+    }
+    @Test
+    public void testOptical() {
+        TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object() {
+        }.getClass().getEnclosingMethod().getName()));
+        mainPage.checkOptical();
+    }
+    @Test
+    public void testPharmacy() {
+        TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object() {
+        }.getClass().getEnclosingMethod().getName()));
+        mainPage.checkPharmacy();
+    }
+    @Test
+    public void testServices() {
+        TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object() {
+        }.getClass().getEnclosingMethod().getName()));
+        mainPage.checkServices();
+    }
+    @Test
+    public void testPhoto() {
+        TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object() {
+        }.getClass().getEnclosingMethod().getName()));
+        mainPage.checkPhoto();
+    }
+    @Test
+    public void testTravel() {
+        TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object() {
+        }.getClass().getEnclosingMethod().getName()));
+        mainPage.checkTravel();
+    }
+    @Test
+    public void testMembership() {
+        TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object() {
+        }.getClass().getEnclosingMethod().getName()));
+        mainPage.checkMembership();
+    }
+    @Test
+    public void testLocation() {
+        TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object() {
+        }.getClass().getEnclosingMethod().getName()));
+        mainPage.checkLocation();
+    }
+    @Test
+    public void customerService() {
+        TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object() {
+        }.getClass().getEnclosingMethod().getName()));
+        mainPage.customerService();
+    }
+    @Test
+    public void getHelp() {
+        TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object() {
+        }.getClass().getEnclosingMethod().getName()));
+        mainPage.getHelp();
+    }
+    @Test
+    public void findaWarehouse() {
+        TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object() {
+        }.getClass().getEnclosingMethod().getName()));
+        mainPage.findaWarehouse();
+    }
+    @Test
+    public void getGetEmailOffers() {
+        TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object() {
+        }.getClass().getEnclosingMethod().getName()));
+        mainPage.getGetEmailOffers();
+    }
+    @Test
+    public void cityStateZip() {
+        TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object() {
+        }.getClass().getEnclosingMethod().getName()));
+        mainPage.cityStateZip();
+    }
+    @Test
+    public void enterYourEmail() {
+        TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object() {
+        }.getClass().getEnclosingMethod().getName()));
+        mainPage.enterYourEmail();
+    }
+    @Test
+    public void followUs() {
+        TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object() {
+        }.getClass().getEnclosingMethod().getName()));
+        mainPage.followUs();
+    }
+    @Test
+    public void facebookLogo() {
+        TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object() {
+        }.getClass().getEnclosingMethod().getName()));
+        mainPage.facebookLogo();
+    }
+    @Test
+    public void pLogo() {
+        TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object() {
+        }.getClass().getEnclosingMethod().getName()));
+        mainPage.pLogo();
+    }
+    @Test(enabled = true) //we should follow it
+    public void pLog() {
+        TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object() {
+        }.getClass().getEnclosingMethod().getName()));
+        mainPage.pLogo();
+    }
+
+
 }
